d48efda025cacedd62a3dce696565dd99ef195fb
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2024-11-22 15:41:08 -0300

    adding productcontroller

---
+++ poc.fullstack.guitarshop.api/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using poc.fullstack.guitarshop.api.Data;
+using poc.fullstack.guitarshop.api.Entities;
+
+namespace poc.fullstack.guitarshop.api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public sealed class ProductController : ControllerBase
+{
+    private readonly ILogger<ProductController> _logger;
+    private readonly GuitarShopContext _context;
+
+    public ProductController(GuitarShopContext context, ILogger<ProductController> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IList<Product>>> GetProductsAsync(CancellationToken ct)
+    {
+        var products = await _context.Products.ToListAsync(ct);
+
+        if (products is null)
+            return NotFound();
+
+        return Ok(products);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<Product>> GetProductByIdAsync(Guid id, CancellationToken ct)
+    {
+        var product = await _context.Products.FindAsync(id, ct);
+        
+        if (product is null) 
+            return NotFound();
+
+        return Ok(product);
+    }
+}
