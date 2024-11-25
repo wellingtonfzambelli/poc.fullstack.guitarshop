3b1ae1574b68192fad1b03918a253b7fb06140ad
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2024-11-24 23:19:12 -0300

    creating product details using axios

--- poc.fullstack.guitarshop.frontend/src/components/catalog/ProductCard.tsx
+++ poc.fullstack.guitarshop.frontend/src/components/catalog/ProductCard.tsx
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Product } from '../../app/models/Product';
 import { Avatar, CardHeader } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 interface Props{
     product: Product;
@@ -36,7 +37,7 @@ export default function ProductCard({product}: Props){
             </CardContent>
             <CardActions>
                 <Button size="small">Add to cart</Button>
-                <Button size="small">View</Button>
+                <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
         </Card>
     );
