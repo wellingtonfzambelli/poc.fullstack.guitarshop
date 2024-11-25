5437734646e3363996a17af436b01e864eff9604
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2024-11-25 16:08:25 -0300

    add validation errors

--- poc.fullstack.guitarshop.frontend/src/components/error/ErrorPage.tsx
+++ poc.fullstack.guitarshop.frontend/src/components/error/ErrorPage.tsx
@@ -1,9 +1,18 @@
 
-import { Container, ButtonGroup, Button } from "@mui/material";
+import { Container, ButtonGroup, Button, Alert, AlertTitle, List, ListItem, ListItemText } from "@mui/material";
 import APIs from "../../services/apis";
 import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react";
 
 export default function ErrorPage(){
+    const [validationErrors, setValidationErrors] = useState<string[]>([]);
+
+    function getValidationError() {
+        APIs.ApiTestErrors.getValidationError()
+            .then(() => console.log('should not see this'))
+            .catch(error => setValidationErrors(error));
+    }
+
     return (        
         <Container>
             <h2>Error for testing purposes</h2>
@@ -12,8 +21,22 @@ export default function ErrorPage(){
                 <Button variant="contained" onClick={() => APIs.ApiTestErrors.getNotFound()}>Test Not Found</Button>
                 <Button variant="contained" onClick={() => APIs.ApiTestErrors.getServerError()}>Test Server Error</Button>
                 <Button variant="contained" onClick={() => APIs.ApiTestErrors.getUnauthorized()}>Test Unauthorized</Button>
-                <Button variant="contained" onClick={() => APIs.ApiTestErrors.getValidationError()}>Test Validation Error</Button>
+                <Button variant="contained" onClick={getValidationError}>Test Validation Error</Button>
             </ButtonGroup>
+
+            {
+                validationErrors.length > 0 && 
+                <Alert severity='error'>
+                    <AlertTitle>Validation Errors</AlertTitle>
+                    <List>
+                        {validationErrors.map(error => (
+                            <ListItem key={error}>
+                                <ListItemText>{error}</ListItemText>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Alert>
+            }
         </Container>
     )
 }
--- poc.fullstack.guitarshop.frontend/src/services/apis.ts
+++ poc.fullstack.guitarshop.frontend/src/services/apis.ts
@@ -10,6 +10,16 @@ axios.interceptors.response.use(async response =>{
 
     switch(status){
         case 400:
+            if(data.errors){
+                const modelStateErrors: string[] = [];
+                for(const key in data.errors){
+                    if(data.errors[key]){
+                        modelStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modelStateErrors.flat();
+            }
+
             toast.error(data.title);
             break;
         case 401:
